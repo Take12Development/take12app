9faa2613cfda9d077f627e9805e5d1f58f99234d
Claudia Calderas <claudia.calderas@gmail.com>
2017-08-22 13:39:26 -0500

    added text editor for story

--- client/scripts/clientapp.js
+++ client/scripts/clientapp.js
@@ -1,8 +1,36 @@
-var take12App = angular.module('take12App', ['ngRoute','ngMaterial','ngFileUpload']);
+var take12App = angular.module('take12App', ['ngRoute','ngMaterial','ngFileUpload','textAngular']);
 
 // Angular Material Theme Configuration
 take12App.config(['$mdThemingProvider', function($mdThemingProvider) {
    $mdThemingProvider.theme('altTheme').primaryPalette('grey').accentPalette('blue-grey');
+}]);
+
+take12App.config(['$provide', function($provide){
+	// this demonstrates how to register a new tool and add it to the default toolbar
+	$provide.decorator('taOptions', ['$delegate', function(taOptions){
+		// $delegate is the taOptions we are decorating
+		// here we override the default toolbars and classes specified in taOptions.
+		taOptions.forceTextAngularSanitize = true; // set false to allow the textAngular-sanitize provider to be replaced
+		taOptions.keyMappings = []; // allow customizable keyMappings for specialized key boards or languages
+		taOptions.toolbar = [
+			// ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'pre', 'quote'],
+			// ['bold', 'italics', 'underline', 'ul', 'ol', 'redo', 'undo', 'clear'],
+      ['bold', 'italics', 'underline', 'ul', 'ol','justifyLeft','justifyCenter','justifyRight', 'justifyFull'],
+			// ['justifyLeft','justifyCenter','justifyRight', 'justifyFull'],
+			// ['html', 'insertImage', 'insertLink', 'wordcount', 'charcount']
+		];
+		taOptions.classes = {
+			focussed: 'focussed',
+			toolbar: 'btn-toolbar',
+			toolbarGroup: 'btn-group',
+			toolbarButton: 'btn btn-default',
+			toolbarButtonActive: 'active',
+			disabled: 'disabled',
+			textEditor: 'form-control',
+			htmlEditor: 'form-control'
+		};
+		return taOptions; // whatever you return will be the taOptions
+	}]);
 }]);
 
 /// Routes ///
