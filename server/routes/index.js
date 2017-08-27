de8c7c3ff1c9d0a2156da1d506640091d7e6f0bf
Betsy Rowley <BRowley8@gmail.com>
2017-08-27 10:11:52 -0500

    Fixes index route

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,14 +8,14 @@ router.post('/',
     passport.authenticate('local', {
         // request stays within node/express and is routed as a new request
         successRedirect: '/user',   // goes to routes/user.js
-        failureRedirect: '/error'   
+        failureRedirect: '/error'
     })
 );
 
 // Handle index file separately
 // Also catches any other request not explicitly matched elsewhere
 router.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '../public/views/index.html'));
+  res.sendFile(path.join(__dirname, '../server/public/views/index.html'));
 });
 
 module.exports = router;
