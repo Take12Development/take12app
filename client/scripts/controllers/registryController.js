c134357581fe6e6412e53ab99a0d9fc2aa1705db
Claudia Calderas <claudia.calderas@gmail.com>
2017-08-29 12:25:14 -0500

    validated when url does not exist in the db

--- client/scripts/controllers/registryController.js
+++ client/scripts/controllers/registryController.js
@@ -6,62 +6,70 @@ take12App.controller('RegistryController', ['$scope', '$http', '$routeParams',
 
   console.log('params is:', $routeParams.registryUrl);
 
+  $scope.validRegistry = true;
 
   // Calls Factory function that gets registry information from the database
   RegistryDataService.getRegistry($routeParams.registryUrl).then(function(data){
 
     console.log('back from server with:', data);
-    $scope.currentRegistry = data.data;
-    $scope.currentRegistry.firstName = UtilitiesService.titleCase($scope.currentRegistry.firstName);
-    $scope.currentRegistry.lastName = UtilitiesService.titleCase($scope.currentRegistry.lastName);
-    $scope.numberOfComments = $scope.currentRegistry.comments.length;
-    console.log('$scope.currentRegistry',$scope.currentRegistry);
+    if (data.data != "") {
+      $scope.validRegistry = true;
+      $scope.currentRegistry = data.data;
+      $scope.currentRegistry.firstName = UtilitiesService.titleCase($scope.currentRegistry.firstName);
+      $scope.currentRegistry.lastName = UtilitiesService.titleCase($scope.currentRegistry.lastName);
+      $scope.numberOfComments = $scope.currentRegistry.comments.length;
+      console.log('$scope.currentRegistry',$scope.currentRegistry);
+      
+      // PIE Chart
+      var ctx = "myChart";
+      var myStaticChart = new Chart(ctx, {
+          type: 'pie',
+          data: {
+                  labels: ["Days Left", "Days Provided", "Days Gifted"],
+                  datasets: [{
+                    backgroundColor: [
+                        "#dedede",
+                        "#6acbc4",
+                        "#f7aca0"
+                    ],
+                    hoverBackgroundColor: [
+                        "#dedede",
+                        "#6acbc4",
+                        "#f7aca0"
+                    ],
+                    data: [20, 50, 30]
+                  }]
+              },
+            options: {
+              legend: {
+                  display: true,
+                  position: 'bottom',
+                  labels: {
+                      fontColor: 'rgb(255, 99, 132)'
+                  }
+              }
+            }
+        }); // End of Pie Chart
+
+    } else {
+      $scope.validRegistry = false;
+    }
+    console.log('Valid Registry?', $scope.validRegistry);
   })
   .catch(function(response){
       console.log(response.status);
   });
 
-  // PIE Chart
-  var ctx = "myChart";
-  var myStaticChart = new Chart(ctx, {
-      type: 'pie',
-      data: {
-              labels: ["Days Left", "Days Provided", "Days Gifted"],
-              datasets: [{
-                backgroundColor: [
-                    "#dedede",
-                    "#6acbc4",
-                    "#f7aca0"
-                ],
-                hoverBackgroundColor: [
-                    "#dedede",
-                    "#6acbc4",
-                    "#f7aca0"
-                ],
-                data: [20, 50, 30]
-              }]
-          },
-        options: {
-          legend: {
-              display: true,
-              position: 'bottom',
-              labels: {
-                  fontColor: 'rgb(255, 99, 132)'
-              }
-          }
-        }
-    });
-
-    // Slider
-    $scope.slider = {
-      minValue: 15,
-      options: {
-        floor: 0,
-        ceil: 500,
-        step: 15,
-        showTicks: false,
-        hidePointerLabels: true,
-      }
-    };
+  // Slider
+  $scope.slider = {
+    minValue: 15,
+    options: {
+      floor: 0,
+      ceil: 500,
+      step: 15,
+      showTicks: false,
+      hidePointerLabels: true,
+    }
+  };
 
 }]);
