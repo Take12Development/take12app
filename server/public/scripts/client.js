b55e6669e36bc8a0b14b1cd128f627dc012281d4
Betsy Rowley <BRowley8@gmail.com>
2017-09-03 10:25:19 -0500

    Adds untracked files

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,4 +1,4 @@
-var take12App = angular.module('take12App', ['ngRoute','ngMaterial','ngFileUpload','textAngular']);
+var take12App = angular.module('take12App', ['ngRoute','ngMaterial','ngFileUpload','textAngular','rzModule']);
 
 // Angular Material Theme Configuration
 take12App.config(['$mdThemingProvider', function($mdThemingProvider) {
@@ -90,6 +90,23 @@ take12App.config(['$routeProvider', '$locationProvider',
         }]
       }
     })
+    // Public registry
+    .when('/registry/:registryUrl', {
+      templateUrl: '/views/templates/registry.html',
+      controller: 'RegistryController',
+    })
+    // Find registry
+    .when('/findRegistry', {
+      templateUrl: '/views/templates/findRegistry.html',
+      controller: 'FindRegistryController',
+      controllerAs: 'vm'
+    })
+    // Public registry
+    .when('/publicRegistry', {
+      templateUrl: '/views/templates/publicRegistry.html',
+      controller: 'FindRegistryController',
+      controllerAs: 'vm'
+    })
     .otherwise({
       redirectTo: 'home'
     });
@@ -97,12 +114,145 @@ take12App.config(['$routeProvider', '$locationProvider',
 
 take12App.controller('DashboardController', ['$scope', '$http',
                       '$window', '$timeout', 'Upload', 'UserService',
-                      'UtilitiesService', 'RegistryDataService',
+                      'UtilitiesService', 'RegistryDataService', '$mdDialog',
                     function($scope, $http, $window, $timeout, Upload,
-                    UserService, UtilitiesService, RegistryDataService) {
+                    UserService, UtilitiesService, RegistryDataService, $mdDialog) {
+
+$scope.validRegistry = true;
+
+$scope.dashboardRegistry = UserService.userObject.currentRegistry;
+
+// list of states
+$scope.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
+    'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
+    'WY').split(' ').map(function(state) {
+        return {abbrev: state};
+});
+
+// validates that there is information to display
+if ($scope.dashboardRegistry) {
+  $scope.fullURL = REGISTRY_URL + $scope.dashboardRegistry.registryURL;
+  $scope.dashboardRegistry.firstName = UtilitiesService.titleCase($scope.dashboardRegistry.firstName);
+  $scope.dashboardRegistry.lastName = UtilitiesService.titleCase($scope.dashboardRegistry.lastName);
+} else {
+  $scope.validRegistry = false;
+};
+
+$scope.customFullscreen = false;
+
+// Change Photo dialog
+$scope.showPhotoDialog = function(ev) {
+  $mdDialog.show({
+    controller: DialogController,
+    templateUrl: 'views/partials/changePhotoDialog.html',
+    locals: {
+      photoURL: ''
+    },
+    parent: angular.element(document.body),
+    targetEvent: ev,
+    clickOutsideToClose:true,
+    fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+  })
+  .then(function(response) {
+    console.log('.then',response);
+    if(response != 'cancel') {
+      if(response != '') {
+        $scope.dashboardRegistry.imageURL = response;
+        console.log('OK $scope.dashboardRegistry.imageURL',$scope.dashboardRegistry.imageURL);
+      }
+    }
+  }, function() {
+    console.log('Dialog canceled');
+  });
+};
+
+// Controls functionality of modal window for photo changing
+function DialogController($scope, $mdDialog, photoURL ) {
+  var dialogCtrl = this;
+  dialogCtrl.photoURL = photoURL;
+
+  // dialog select and close function (triggered by ok button)
+  $scope.selectAndClose = function() {
+    $mdDialog.hide(dialogCtrl.photoURL);
+  };
+
+  // dialog cancel function
+  $scope.cancel = function() {
+    $mdDialog.cancel('cancel');
+  };
+
+  // Upload picture file Section
+  var filename;
+  $scope.uploadPic = function(file) {
+    console.log('uploadPic()',file);
+    file.upload = Upload.upload({
+      url: '/uploads',
+      // data: {name: UserService.userObject.email, file: file},
+      data: {file: file},
+    });
+
+    file.upload.then(function (response) {
+      console.log('0 Back from upload with data:',response.data);
+      // saves filename to use when saving registry
+      // filename in localhost:
+      filename = response.data.file.path + "/" + response.data.file.originalname;
+      // updated filename that works with aws
+      // filename = response.data.file.location;
+      dialogCtrl.photoURL = filename;
+
+      $timeout(function () {
+        file.result = response.data;
+        console.log('1 Back from upload with data:',response.data);
+        // filename in localhost:
+        filename = response.data.file.path + "/" + response.data.file.originalname;
+        // updated filename that works with aws
+        // filename = response.data.file.location;
+        dialogCtrl.photoURL = filename;
+      });
+      }, function (response) {
+        if (response.status > 0)
+          $scope.errorMsg = response.status + ': ' + response.data;
+          console.log('2 Back from upload with data:',response.data);
+          console.log('URL is:',filename);
+      }, function (evt) {
+        // Math.min is to fix IE which reports 200% sometimes
+        file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+      });
+    };
+  } // end of DialogController
+
+  // Discard Changes
+  $scope.discardChanges = function() {
+    // go to main page
+    UtilitiesService.redirect('/main');
+  }
+
+  // Updates registry in the DB
+  $scope.saveChanges = function() {
+    console.log('Dashboard Registry:', $scope.dashboardRegistry);
+    RegistryDataService.updateRegistry($scope.dashboardRegistry);
+
+    // go to main page
+    UtilitiesService.redirect('/main');
+  }
 
 
 }]);
+
+take12App.controller('FindRegistryController', ['$http',
+                      'RegistryDataService',
+  function($http, RegistryDataService) {
+
+  //controller reference
+  var vm = this;
+  // Calls Factory Function that GETS all registries from the db
+  vm.getRegistries = RegistryDataService.getRegistries;
+  //Stores all registries from db
+  vm.registriesObject = RegistryDataService.registriesObject;
+  //Limit for the string length of the story
+  vm.limit = 200;
+
+  }]);
 
 take12App.controller('LoginController', ['$scope', '$http', '$location', 'UserService',
                     function($scope, $http, $location, UserService) {
@@ -122,12 +272,14 @@ take12App.controller('LoginController', ['$scope', '$http', '$location', 'UserSe
       $http.post('/', $scope.user).then(function(response) {
         if(response.data.email) {
           UserService.userObject.email = response.data.email;
+          UserService.userObject.registries = response.data.registries;
           console.log('success: ', response.data);
-          if(response.data.registryURL && response.data.registryURL != "") {
+          if(response.data.registries.length != 0) {
             // Existing user: Presents registry dashboard
             $location.path('/main');
           } else {
-            // New user: Presents register views
+            // New user: Presents registration views
+            // $location.path('/registration',response.data);
             $location.path('/registration');
           }
         } else {
@@ -141,12 +293,44 @@ take12App.controller('LoginController', ['$scope', '$http', '$location', 'UserSe
 }]);
 
 take12App.controller('MainController', ['$scope', 'UserService', 'UtilitiesService',
-                                    function($scope, UserService, UtilitiesService) {
+                      'RegistryDataService',
+                      function($scope, UserService, UtilitiesService, RegistryDataService) {
   $scope.userObject = UserService.userObject;
   $scope.logout = UserService.logout;
   $scope.redirect = UtilitiesService.redirect;
+  $scope.userRegistries = RegistryDataService.registriesObject.userRegistries;
 
   console.log('userObject:', $scope.userObject);
+
+  var registriesArray = $scope.userObject.registries;
+  console.log('registriesArray', registriesArray);
+
+  // Calls Factory function that gets registries information for current user from the database
+  RegistryDataService.getUserRegistries($scope.userObject.registries).then(function(data){
+
+    console.log('back from server with:', data);
+    if (data.data != "") {
+      $scope.userRegistries = RegistryDataService.registriesObject.userRegistries;
+
+      console.log('factory', RegistryDataService.registriesObject.userRegistries);
+      console.log('scope',$scope.userRegistries);
+    }
+  })
+  .catch(function(response){
+      console.log(response.status);
+  });
+
+  $scope.newRegistry = function() {
+    UtilitiesService.redirect('/registration')
+  };
+
+  $scope.goToRegistryDashboard = function(registry) {
+    UserService.userObject.currentRegistry = angular.copy(registry);
+    UtilitiesService.redirect('/dashboard');
+  };
+
+
+
 
 }]);
 
@@ -173,7 +357,7 @@ take12App.controller('RegistrationController', ['$scope', '$http',
   $scope.newUser = {
     email: '',
     password: '',
-    registryURL: ''
+    registries: []
   };
 
   // new registry information
@@ -213,7 +397,7 @@ take12App.controller('RegistrationController', ['$scope', '$http',
   // opens how to plan information on separate window
   $scope.goToHowToPlan = function() {
     // THIS HAS TO BE REPLACED WITH CORRECT URL
-    $window.open('http://localhost:5000/#/howToPlan', '_blank');
+    $window.open(HOW_TO_PLAN_URL, '_blank');
   };
 
   // compares pasword and password confirmation entered by the user
@@ -311,13 +495,94 @@ $scope.uploadPic = function(file) {
 
 }]);
 
-take12App.factory('RegistryDataService', ['$http', function($http){
+take12App.controller('RegistryController', ['$scope', '$http', '$routeParams',
+                      '$window', '$timeout', 'Upload', 'UserService',
+                      'UtilitiesService', 'RegistryDataService',
+                    function($scope, $http, $routeParams, $window, $timeout, Upload,
+                    UserService, UtilitiesService, RegistryDataService) {
+
+  console.log('params is:', $routeParams.registryUrl);
+
+  $scope.validRegistry = true;
+
+  // Calls Factory function that gets registry information from the database
+  RegistryDataService.getRegistry($routeParams.registryUrl).then(function(data){
+
+    console.log('back from server with:', data);
+    if (data.data != "") {
+      $scope.validRegistry = true;
+      $scope.currentRegistry = data.data;
+      $scope.currentRegistry.firstName = UtilitiesService.titleCase($scope.currentRegistry.firstName);
+      $scope.currentRegistry.lastName = UtilitiesService.titleCase($scope.currentRegistry.lastName);
+      $scope.numberOfComments = $scope.currentRegistry.comments.length;
+      console.log('$scope.currentRegistry',$scope.currentRegistry);
+      
+      // PIE Chart
+      var ctx = "myChart";
+      var myStaticChart = new Chart(ctx, {
+          type: 'pie',
+          data: {
+                  labels: ["Days Left", "Days Provided", "Days Gifted"],
+                  datasets: [{
+                    backgroundColor: [
+                        "#dedede",
+                        "#6acbc4",
+                        "#f7aca0"
+                    ],
+                    hoverBackgroundColor: [
+                        "#dedede",
+                        "#6acbc4",
+                        "#f7aca0"
+                    ],
+                    data: [20, 50, 30]
+                  }]
+              },
+            options: {
+              legend: {
+                  display: true,
+                  position: 'bottom',
+                  labels: {
+                      fontColor: 'rgb(255, 99, 132)'
+                  }
+              }
+            }
+        }); // End of Pie Chart
+
+    } else {
+      $scope.validRegistry = false;
+    }
+    console.log('Valid Registry?', $scope.validRegistry);
+  })
+  .catch(function(response){
+      console.log(response.status);
+  });
+
+  // Slider
+  $scope.slider = {
+    minValue: 15,
+    options: {
+      floor: 0,
+      ceil: 500,
+      step: 15,
+      showTicks: false,
+      hidePointerLabels: true,
+    }
+  };
+
+}]);
+
+const REGISTRY_URL = 'http://localhost:5000/#/registry/';
+const HOW_TO_PLAN_URL = 'http://localhost:5000/#/howToPlan';
+
+take12App.factory('RegistryDataService', ['$http','$q', 'UserService',
+                  function($http,$q,UserService) {
 
   console.log('Registry Data Service Loaded');
 
   // Stores all registries in the DB
   var registriesObject = {
-    allRegistries: []
+    allRegistries: [],
+    userRegistries: []
   };
 
   // Gets all registries in the database
@@ -329,22 +594,49 @@ take12App.factory('RegistryDataService', ['$http', function($http){
     });
   };
 
-  // Gets a specific registry from the database
-  getRegistry = function(){
-    $http.get('/registry').then(function(response) {
-      console.log('Back from the server with:', response);
-      registriesObject.allRegistries = response.data;
-      console.log('Updated registriesObject:', registriesObject.allRegistries);
+  // function that uses a promise to handle the $http call to get
+  // the registry item from the database
+  getRegistry = function(registryURL) {
+    var deferred = $q.defer();
+    $http.get('/registry/' + registryURL)
+    .then(function(response) {
+        deferred.resolve(response);
+    })
+    .catch(function(response) {
+      deferred.reject(response);
     });
+    return deferred.promise;
+  };
+
+  // gets all registries that are part of array received as a parameter
+  getUserRegistries = function(arrayOfRegistries) {
+    var deferred = $q.defer();
+    $http.post('/registry/getuserregistries', {registries: arrayOfRegistries})
+    .then(function(response) {
+        deferred.resolve(response);
+        registriesObject.userRegistries = response.data;
+    })
+    .catch(function(response) {
+      deferred.reject(response);
+    });
+    return deferred.promise;
   };
 
   // Posts a new registry to the database
   postRegistry = function(registry) {
+    var deferred = $q.defer();
     var registryToPost = angular.copy(registry);
-    console.log('Posting registry: ', registryToPost);
-    $http.post('/registry/add', registryToPost).then(function(response) {
-      console.log('success:',response);
+
+    $http.post('/registry/add', registryToPost)
+    .then(function(response) {
+        deferred.resolve(response);
+        console.log('Back from POST with', response.data);
+        UserService.userObject.currentRegistry = angular.copy(response.data);
+    })
+    .catch(function(response) {
+      deferred.reject(response);
     });
+    return deferred.promise;
   };
 
   // Updates a specific registry
@@ -360,6 +652,7 @@ take12App.factory('RegistryDataService', ['$http', function($http){
     registriesObject : registriesObject,
     getRegistries : getRegistries,
     getRegistry : getRegistry,
+    getUserRegistries : getUserRegistries,
     postRegistry : postRegistry,
     updateRegistry : updateRegistry
   };
@@ -372,6 +665,7 @@ take12App.factory('UserService', ['$http', '$location', function($http, $locatio
   // Stores logged user information
   var userObject = {};
 
+
   return {
     userObject : userObject,
     // Gets logged user
@@ -380,7 +674,7 @@ take12App.factory('UserService', ['$http', '$location', function($http, $locatio
         if(response.data.email) {
           // user has a curret session on the server
           userObject.email = response.data.email;
-          userObject.registryURL = response.data.registryURL;
+          userObject.registries = response.data.registries;
           console.log('User Data: ', userObject);
         } else {
           // user has no session, bounce them back to the login page
@@ -412,6 +706,10 @@ take12App.factory('UtilitiesService', ['$mdDialog', '$location',
       );
   };
 
+  titleCase = function (stringParam) {
+    return stringParam.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+  };
+
   // Redirects to view received as a parameter
   function redirect(page) {
     $location.url(page);
@@ -419,7 +717,8 @@ take12App.factory('UtilitiesService', ['$mdDialog', '$location',
 
 return {
     showAlert: showAlert,
-    redirect : redirect
+    redirect : redirect,
+    titleCase : titleCase
 };
 
 }]);
