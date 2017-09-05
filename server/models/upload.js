5d2a293a6d0739df5cbb713a1c5e5e58ab7bbc0d
Claudia Calderas <claudia.calderas@gmail.com>
2017-09-05 13:09:20 -0500

    deploying to heroku

--- server/models/upload.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-// Picture upload Schema
-var UploadSchema = mongoose.Schema({
-  created: Date,
-  file: Object
-});
-
-module.exports = mongoose.model('Upload', UploadSchema);
