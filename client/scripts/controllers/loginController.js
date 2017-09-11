62b7d1a7a5b5b8c538d74678e37932ab17ad1730
Claudia Calderas <claudia.calderas@gmail.com>
2017-09-11 10:26:44 -0500

    moved fblogin to loginController, login with fb working

--- client/scripts/controllers/loginController.js
+++ client/scripts/controllers/loginController.js
@@ -1,5 +1,6 @@
-take12App.controller('LoginController', ['$scope', '$http', '$location', 'UserService',
-                    function($scope, $http, $location, UserService) {
+take12App.controller('LoginController', ['$scope', '$http', 'UserService',
+                    'UtilitiesService',
+                    function($scope, $http, UserService, UtilitiesService) {
 
   $scope.user = {
     email: '',
@@ -10,7 +11,7 @@ take12App.controller('LoginController', ['$scope', '$http', '$location', 'UserSe
 
   // Facebook methods in UserService:
   $scope.checkLoginState = UserService.checkLoginState;
-  $scope.fblogin = UserService.fblogin;
+  // $scope.fblogin = UserService.fblogin;
 
   // logs a user into the system
   $scope.login = function() {
@@ -24,11 +25,10 @@ take12App.controller('LoginController', ['$scope', '$http', '$location', 'UserSe
           console.log('success: ', response.data);
           if(response.data.registries.length != 0) {
             // Existing user: Presents registry dashboard
-            $location.path('/main');
+            UtilitiesService.redirect('/main');
           } else {
             // New user: Presents registration views
-            // $location.path('/registration',response.data);
-            $location.path('/registration');
+            UtilitiesService.redirect('/registration');
           }
         } else {
           console.log('failure: ', response);
@@ -37,5 +37,44 @@ take12App.controller('LoginController', ['$scope', '$http', '$location', 'UserSe
       });
     }
   };
+
+  // logs a facebook user into the system
+  $scope.fblogin = function() {
+    console.log('LOGIN CONTROLLER fblogin');
+    FB.login(function(response) {
+      if (response.authResponse) {
+        console.log('LC: Welcome!  Fetching your information.... ');
+        FB.api('/me', function(response) {
+          console.log(response);
+          console.log('US: Good to see you, ' + response.name + '.');
+          var token = FB.getAuthResponse().accessToken;
+
+          // $http.post('fblogin/auth/facebook/token?access_token=' + token, sendData).then(handleSuccess, handleFailure);
+          $http.post('fblogin/auth/facebook/token?access_token=' + token).then(handleSuccess, handleFailure);
+
+          function handleSuccess(response) {
+            console.log('LC CONTROLLER: created or found FB user', response.data);
+            if(response.data.email) {
+              UserService.userObject.email = response.data.email;
+              UserService.userObject.registries = response.data.registries;
+              if(response.data.registries.length != 0) {
+                // Existing user: Presents registry dashboard
+                UtilitiesService.redirect('/main');
+              } else {
+                // New user: Presents registration views
+                UtilitiesService.redirect('/registration');
+              }
+            }
+          };
+          function handleFailure(response) {
+            console.log('facebook logged Failure Logging In', response);
+          };
+        });
+      } else {
+        console.log('User cancelled login or did not fully authorize.');
+      }
+    });
+  };
+
 
 }]);
--- client/scripts/factories/UserService.js
+++ client/scripts/factories/UserService.js
@@ -1,5 +1,5 @@
-take12App.factory('UserService', ['$http', '$location', 'UtilitiesService',
-                  function($http, $location, UtilitiesService){
+take12App.factory('UserService', ['$http', 'UtilitiesService',
+                  function($http, UtilitiesService){
 
   // Stores logged user information
   var userObject = {};
@@ -18,7 +18,7 @@ take12App.factory('UserService', ['$http', '$location', 'UtilitiesService',
         userObject.registries = response.data.registries;
       } else {
         // user has no session, bounce them back to the login page
-        $location.path("/home");
+        UtilitiesService.redirect("/home");
       }
     });
   };
@@ -27,105 +27,40 @@ take12App.factory('UserService', ['$http', '$location', 'UtilitiesService',
   function logout() {
     $http.get('/user/logout').then(function(response) {
       console.log('logged out');
-      $location.path("/home");
+      UtilitiesService.redirect("/home");
     });
   };
 
-///////////////////////////////////////////////////////////////////////////////
-
-// facebook login
-function fblogin() {
-  console.log('In fblogin');
-  FB.login(function(response) {
-    if (response.authResponse) {
-    console.log('USER SERVICE: Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-      console.log(response);
-      console.log('US: Good to see you, ' + response.name + '.');
-      var token = FB.getAuthResponse().accessToken;
-
-      if(response.name) {
-          var fullName = response.name;
+  // Checks Facebook login status
+  function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      if (response.status === 'connected') {
+        // the user is logged in and has authenticated your
+        // app, and response.authResponse supplies
+        // the user's ID, a valid access token, a signed
+        // request, and the time the access token
+        // and signed request each expire
+        var uid = response.authResponse.userID;
+        var accessToken = response.authResponse.accessToken;
+        console.log('Already logged into facebook, here is your data', response);
+      } else if (response.status === 'not_authorized') {
+        // the user is logged in to Facebook,
+        // but has not authenticated your app
+        console.log('Not logged in but i can handle that 1', response);
+        fblogin();
       } else {
-          var fullName = "UPDATE NAME";
+        // the user isn't logged in to Facebook.
+        console.log('Not logged in but i can handle that 2', response);
+        fblogin();
       }
-
-      // console.log(fullName);
-      var fName = fullName.split(' ').slice(0, -1).join(' ');
-      var lName = fullName.split(' ').slice(-1).join(' ');
-
-      var lowerFirst = fName.toLowerCase();
-      var lowerLast = lName.toLowerCase();
-
-      console.log(lowerFirst, lowerLast);
-
-      var sendData = {};
-      sendData.facebookid = response.id;
-      sendData.firstName = lowerFirst;
-      sendData.lastName = lowerLast;
-      sendData.email = response.email;
-
-      console.log('US: sendData',sendData);
-
-      // $http.post('fblogin/auth/facebook/token?access_token=' + token, sendData).then(handleSuccess, handleFailure);
-      $http.post('fblogin/auth/facebook/token?access_token=' + token).then(handleSuccess, handleFailure);
-
-      function handleSuccess(response) {
-        console.log('USERSERVICE CONTROLLER: created or found FB user', response.data);
-        if(response.data.registries.length != 0) {
-          // Existing user: Presents registry dashboard
-          UtilitiesService.redirect('/main');
-          console.log('MAIN');
-        } else {
-          // New user: Presents registration views
-          UtilitiesService.redirect('/registration');
-          console.log('REGISTRATION');
-        }
-      };
-      function handleFailure(response) {
-        console.log('facebook logged Failure Logging In', response);
-      };
-
-     });
-    } else {
-    //  console.log('User cancelled login or did not fully authorize.');
-    }
-  });
-}
-
-
-// Checks Facebook login status
-function checkLoginState() {
-  FB.getLoginStatus(function(response) {
-    if (response.status === 'connected') {
-      // the user is logged in and has authenticated your
-      // app, and response.authResponse supplies
-      // the user's ID, a valid access token, a signed
-      // request, and the time the access token
-      // and signed request each expire
-      var uid = response.authResponse.userID;
-      var accessToken = response.authResponse.accessToken;
-      console.log('Already logged into facebook, here is your data', response);
-    } else if (response.status === 'not_authorized') {
-      // the user is logged in to Facebook,
-      // but has not authenticated your app
-      console.log('Not logged in but i can handle that 1', response);
-      fblogin();
-    } else {
-      // the user isn't logged in to Facebook.
-      console.log('Not logged in but i can handle that 2', response);
-      fblogin();
-    }
-   }, true);
-};
+     }, true);
+  };
 
 
   return {
     userObject : userObject,
     getuser : getuser,
     logout : logout,
-    checkLoginState : checkLoginState,
-    fblogin : fblogin
-
+    checkLoginState : checkLoginState
   };
 }]);
