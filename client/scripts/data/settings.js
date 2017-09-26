610c223c87963233af2854174207857cfae7ac8f
Betsy Rowley <BRowley8@gmail.com>
2017-09-25 20:14:59 -0500

    Adds constants for production

--- client/scripts/data/settings.js
+++ client/scripts/data/settings.js
@@ -1,2 +1,7 @@
-const FACEBOOK_APP_ID = '1798936316813029';
-const STRIPE_PUBLIC = 'pk_test_ASBdjhXFpe8PhYFPm6edjhkh';
+//For Heroku-Dev
+// const FACEBOOK_APP_ID = '1798936316813029';
+// const STRIPE_PUBLIC = 'pk_test_ASBdjhXFpe8PhYFPm6edjhkh';
+
+//For Heroku-Live
+const FACEBOOK_APP_ID = '1702338590083847';
+const STRIPE_PUBLIC = 'pk_live_0OHSK65Er7kp7Ici9pFoWX4p';
