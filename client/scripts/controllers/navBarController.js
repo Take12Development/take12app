752dfccb2b278c3e4b34b3ba06ff617d7f5c7c05
Claudia Calderas <claudia.calderas@gmail.com>
2017-10-16 21:28:36 -0500

    user log out for passport users

--- client/scripts/controllers/navBarController.js
+++ client/scripts/controllers/navBarController.js
@@ -1,9 +1,18 @@
-take12App.controller('navBarController', ['$scope', '$location','UtilitiesService', 'UserService',
-                      function($scope, $location, UtilitiesService, UserService) {
+take12App.controller('navBarController', ['$scope', '$location','$mdDialog','UtilitiesService', 'UserService',
+                      function($scope, $location, $mdDialog, UtilitiesService, UserService) {
   var originatorEv;
   $scope.redirect = UtilitiesService.redirect;
   $scope.loggedInUser = UserService.userObject.loggedInUser;
   console.log('navBarController', UserService.userObject.loggedInUser);
+
+  // logs in or logs out user
+  $scope.loginout = function() {
+    console.log('navBarController', UserService.userObject.loggedInUser);
+    if (UserService.userObject.loggedInUser == true) {
+      UserService.logout();
+      UtilitiesService.showAlert('You have been logged out of the system');
+    }
+  }
 
   // Displays menu options
   $scope.openMenu = function($mdMenu, ev) {
