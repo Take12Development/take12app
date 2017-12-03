2624cef97e55ebd975950b16422a689168d76239
Betsy Rowley <BRowley8@gmail.com>
2017-12-03 14:19:36 -0600

    Changes settings for heroku-live

--- client/scripts/data/settings.js
+++ client/scripts/data/settings.js
@@ -1,7 +1,7 @@
 //For Heroku-Dev
-const FACEBOOK_APP_ID = '1798936316813029';
-const STRIPE_PUBLIC = 'pk_test_ASBdjhXFpe8PhYFPm6edjhkh';
+// const FACEBOOK_APP_ID = '1798936316813029';
+// const STRIPE_PUBLIC = 'pk_test_ASBdjhXFpe8PhYFPm6edjhkh';
 
 //For Heroku-Live
-// const FACEBOOK_APP_ID = '1702338590083847';
-// const STRIPE_PUBLIC = 'pk_live_0OHSK65Er7kp7Ici9pFoWX4p';
+const FACEBOOK_APP_ID = '1702338590083847';
+const STRIPE_PUBLIC = 'pk_live_0OHSK65Er7kp7Ici9pFoWX4p';
