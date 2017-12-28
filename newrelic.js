a134741e221ddac496483794864cc02e09a2ce8d
Betsy Rowley <BRowley8@gmail.com>
2017-12-28 08:47:17 -0600

    Adds NewRelic via heroku

---
+++ newrelic.js
@@ -0,0 +1,25 @@
+'use strict'
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['take12production'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '6bb05d52b325addd18e74c686478893ea35cf852',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
