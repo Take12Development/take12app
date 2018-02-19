d1cdf4c2f4b1018af9055b38533963a4f2ef4821
Betsy Rowley <BRowley8@gmail.com>
2018-02-19 09:57:39 -0600

    Updates settings for Prod

--- client/scripts/data/settings.js
+++ client/scripts/data/settings.js
@@ -1,7 +1,7 @@
 //For Heroku-Dev
-var FACEBOOK_APP_ID = '1798936316813029';
-var STRIPE_PUBLIC = 'pk_test_ASBdjhXFpe8PhYFPm6edjhkh';
+// var FACEBOOK_APP_ID = '1798936316813029';
+// var STRIPE_PUBLIC = 'pk_test_ASBdjhXFpe8PhYFPm6edjhkh';
 
 //For Heroku-Live
-// var FACEBOOK_APP_ID = '1702338590083847';
-// var STRIPE_PUBLIC = 'pk_live_0OHSK65Er7kp7Ici9pFoWX4p';
+var FACEBOOK_APP_ID = '1702338590083847';
+var STRIPE_PUBLIC = 'pk_live_0OHSK65Er7kp7Ici9pFoWX4p';
